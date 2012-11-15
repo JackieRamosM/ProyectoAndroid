a1cabb86adae28a93cad2ad3cfeedad3776923e9
Denny K. Schuldt <dschuldt@espol.edu.ec>
2012-11-15 00:47:02 -0500

    Toast y Thread agregados.

---
+++ Comic_It/src/comic_it/project/VtnPlantillas.java
@@ -0,0 +1,16 @@
+package comic_it.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ *
+ * @author Denny
+ */
+public class VtnPlantillas extends Activity{    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plantillas);
+    }
+}
