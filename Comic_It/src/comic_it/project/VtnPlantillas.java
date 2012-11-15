3bc417ed9ae41684723bb59970d970525009b1b9
Denny K. Schuldt <dschuldt@espol.edu.ec>
2012-11-15 02:21:27 -0500

    Plantillas de prueba agregadas

--- Comic_It/src/comic_it/project/VtnPlantillas.java
+++ Comic_It/src/comic_it/project/VtnPlantillas.java
@@ -1,16 +1,23 @@
 package comic_it.project;
 
 import android.app.Activity;
+import android.graphics.PorterDuff.Mode;
 import android.os.Bundle;
+import android.widget.Button;
 
 /**
  *
  * @author Denny
  */
 public class VtnPlantillas extends Activity{    
+	
+	Button aceptar;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plantillas);
+        aceptar = (Button) findViewById(R.id.aceptar);
+        aceptar.getBackground().setColorFilter(0xFFFF0000, Mode.MULTIPLY);
     }
 }
