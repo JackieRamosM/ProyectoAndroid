8bc0b34dbfb08780b4c7346d2e2107c2d7d02ad3
Ana Arias <acarias@espol.edu.ec>
2012-11-19 11:23:05 -0500

    Tomando un simbolo escogido

--- Comic_It/src/comic_it/project/P2_symbols.java
+++ Comic_It/src/comic_it/project/P2_symbols.java
@@ -1,12 +1,26 @@
 package comic_it.project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+	
 	public class P2_symbols extends Activity{
+		Button btn_okk;
+		Intent intent;
 		@Override
 	    public void onCreate(Bundle savedInstanceState){    	
 	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.p2_symbols);        	        
+	        setContentView(R.layout.p2_symbols);  
+	        btn_okk= (Button) findViewById(R.id.btn_ok);
+	        btn_okk.setOnClickListener((OnClickListener) this);
     }
+		
+		public void onClick(View arg0) {
+			intent = new Intent(P2_symbols.this,VtnEdicion.class);		
+			intent.putExtra("texto seleccionado",R.id.ivPrincipal);
+		    startActivity(intent); 
+			}
 }
