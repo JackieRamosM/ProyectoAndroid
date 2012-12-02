e0cc303dea850204909d45e7863f33aae3a8d2be
Ana Arias <acarias@espol.edu.ec>
2012-12-02 18:09:39 -0500

    Añadiendo funcionalidades
    Plantillas y Drag and Drop

--- Comic_It/src/comic_it/project/VtnTabs.java
+++ Comic_It/src/comic_it/project/VtnTabs.java
@@ -108,12 +108,336 @@ public class VtnTabs extends TabActivity{
 	 *				los diferentes íconos y/o textos que pueden ser agregados.	 
 	 */
 	public static class P1_comic extends Activity{
+
+			//TODO EL CODIGO DE P1 AQUI	private static final int PICK_FROM_CAMERA = 1;
+		private static final int CROP_FROM_CAMERA = 2;
+		private static final int PICK_FROM_FILE = 3;
+		private int TAKE_PICTURE = 1;
+		private int SELECT_PICTURE = 2;
+		private Uri mImageCaptureUri;		
+		private String name = "";
 		
+		ImageView imagen1,imagen2,imagen3,imagen4,imagen5,imagen6, iv, img1, img2, img3, img;
+		Button btnBurbujas, btn_tomado, compartir, guardar, aceptar, plus;	
+		Boolean cambio1 = false, cambio2 = false, cambio3 = false;
+		ImageView icono1, icono2, icono3;
+		View selected_item = null;
+		Bitmap ima, photo, bitmap;
+		Intent intent;
+		Drawable d;
 		
+		public static ArrayList<ImageView> imagenes = new ArrayList<ImageView>();
+		public static ViewGroup vg1, vg2, vg3, vg4, vg5, vg6, vg7, vg8, vg9, vg10, vg11, vg12, vg13, vg14, vg15;
+		public static ImageView over_1, over_2, over_3, over_4, over_5, over_6, over_7, over_8, over_9, over_10, over_11, over_12, over_13, over_14, over_15;
+				  		
+		int bundle_comic,bundle_camara,i=0,offset_x = 0,offset_y = 0;						
 		
-			//TODO EL CODIGO DE P1 AQUI
+
+		@Override
+	    public void onCreate(Bundle savedInstanceState){
+	        super.onCreate(savedInstanceState);
+	        bundle_comic = getIntent().getExtras().getInt("cargarPlantilla");
+	        
+	        final String [] items = new String [] {"Camara", "Galeria"};				
+			ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.select_dialog_item,items);
+			AlertDialog.Builder builder	= new AlertDialog.Builder(this);
+			
+			builder.setTitle("Seleccion de Imagen");
+			builder.setAdapter(adapter, new DialogInterface.OnClickListener(){
+				public void onClick( DialogInterface dialog, int item ){ 
+					if(item == 0){
+						Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);							
+						mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(),
+										   "Avatar" + String.valueOf(System.currentTimeMillis()) + ".jpg"));
+						intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+						try{								
+							intent.putExtra("return-data", true);
+							startActivityForResult(intent, PICK_FROM_CAMERA);								
+						}catch(ActivityNotFoundException e){
+							e.printStackTrace();
+						}
+					}else{
+						Intent intent = new Intent();							
+		                intent.setType("image/*");
+		                intent.setAction(Intent.ACTION_GET_CONTENT);			                
+		                startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+					}
+				}
+			});
+			
+			final AlertDialog dialog = builder.create();
+			
+	        switch(bundle_comic){
+	        	case R.id.plantilla1:
+	        		setContentView(R.layout.plantilla_1);
+			        imagen1=(ImageView) findViewById(R.id.p1_img1);
+			        imagen2=(ImageView) findViewById(R.id.p1_img2);
+			        imagen3=(ImageView) findViewById(R.id.p1_img3);
+			        imagen4=(ImageView) findViewById(R.id.p1_img4);
+			        imagen5=(ImageView) findViewById(R.id.p1_img5);			        
+			        		        				     		       
+			        imagen1.setOnClickListener(new View.OnClickListener(){				
+						public void onClick(View v){
+							dialog.show();
+							i=1;
+						}
+					});		        
+			        imagen2.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=2;
+						}
+					});
+			        imagen3.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=3;
+						}
+					});
+					imagen4.setOnClickListener(new View.OnClickListener(){							
+						public void onClick(View v) {
+							dialog.show();
+							i=4;
+						}
+					});
+					imagen5.setOnClickListener(new View.OnClickListener(){						
+						public void onClick(View v){
+							dialog.show();
+							i=5;
+						}
+					});	
 		
+					over_1 = (ImageView) findViewById(R.id.vg_icono1_1); imagenes.add(over_1);
+					over_2 = (ImageView) findViewById(R.id.vg_icono2_1); imagenes.add(over_2);
+					over_3 = (ImageView) findViewById(R.id.vg_icono3_1); imagenes.add(over_3);
+					over_4 = (ImageView) findViewById(R.id.vg_icono4_1); imagenes.add(over_4);
+					over_5 = (ImageView) findViewById(R.id.vg_icono5_1); imagenes.add(over_5);
+					over_6 = (ImageView) findViewById(R.id.vg_icono6_1); imagenes.add(over_6);
+					over_7 = (ImageView) findViewById(R.id.vg_icono7_1); imagenes.add(over_7);
+					over_8 = (ImageView) findViewById(R.id.vg_icono8_1); imagenes.add(over_8);
+					over_9 = (ImageView) findViewById(R.id.vg_icono9_1); imagenes.add(over_9);
+					over_10 = (ImageView) findViewById(R.id.vg_icono10_1); imagenes.add(over_10);
+					over_11 = (ImageView) findViewById(R.id.vg_icono11_1); imagenes.add(over_11);
+					over_12 = (ImageView) findViewById(R.id.vg_icono12_1); imagenes.add(over_12);
+					over_13 = (ImageView) findViewById(R.id.vg_icono13_1); imagenes.add(over_13);
+					over_14 = (ImageView) findViewById(R.id.vg_icono14_1); imagenes.add(over_14);
+					over_15 = (ImageView) findViewById(R.id.vg_icono15_1); imagenes.add(over_15);
+										
+					vg1 = (ViewGroup) findViewById(R.id.vg1_1);
+			        	agregarDrag_and_Drop(vg1,over_1);
+			        vg2 = (ViewGroup) findViewById(R.id.vg2_1);
+			        	agregarDrag_and_Drop(vg2,over_2);
+			        vg3 = (ViewGroup) findViewById(R.id.vg3_1);
+			        	agregarDrag_and_Drop(vg3,over_3);
+			        vg4 = (ViewGroup) findViewById(R.id.vg4_1);
+			        	agregarDrag_and_Drop(vg4,over_4);
+			        vg5 = (ViewGroup) findViewById(R.id.vg5_1);
+			        	agregarDrag_and_Drop(vg5,over_5);
+			        vg6 = (ViewGroup) findViewById(R.id.vg6_1);
+			        	agregarDrag_and_Drop(vg6,over_6);
+			        vg7 = (ViewGroup) findViewById(R.id.vg7_1);
+			        	agregarDrag_and_Drop(vg7,over_7);
+			        vg8 = (ViewGroup) findViewById(R.id.vg8_1);
+			        	agregarDrag_and_Drop(vg8,over_8);
+			        vg9 = (ViewGroup) findViewById(R.id.vg9_1);
+			        	agregarDrag_and_Drop(vg9,over_9);
+			        vg10 = (ViewGroup) findViewById(R.id.vg10_1);
+			        	agregarDrag_and_Drop(vg10,over_10);
+			        vg11 = (ViewGroup) findViewById(R.id.vg11_1);
+			        	agregarDrag_and_Drop(vg11,over_11);
+			        vg12 = (ViewGroup) findViewById(R.id.vg12_1);
+			        	agregarDrag_and_Drop(vg12,over_12);
+			        vg13 = (ViewGroup) findViewById(R.id.vg13_1);
+			        	agregarDrag_and_Drop(vg13,over_13);
+			        vg14 = (ViewGroup) findViewById(R.id.vg14_1);
+			        	agregarDrag_and_Drop(vg14,over_14);
+			        vg15 = (ViewGroup) findViewById(R.id.vg15_1);
+			        	agregarDrag_and_Drop(vg15,over_15);			        
+			        			
+	    		break;
+	        	case R.id.plantilla2:
+	        		setContentView(R.layout.plantilla_2);
+			        imagen1=(ImageView) findViewById(R.id.p2_img1);
+			        imagen2=(ImageView) findViewById(R.id.p2_img2);
+			        imagen3=(ImageView) findViewById(R.id.p2_img3);
+			        imagen4=(ImageView) findViewById(R.id.p2_img4);
+			        
+			        imagen1.setOnClickListener(new View.OnClickListener(){				
+						public void onClick(View v){
+							dialog.show();
+							i=1;
+						}
+					});		        
+			        imagen2.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=2;
+						}
+					});
+			        imagen3.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=3;
+						}
+					});
+					imagen4.setOnClickListener(new View.OnClickListener(){							
+						public void onClick(View v) {
+							dialog.show();
+							i=4;
+						}
+					});			
+	    		break;
+	        	case R.id.plantilla3:
+	        		setContentView(R.layout.plantilla_3);
+			        imagen1=(ImageView) findViewById(R.id.p3_img1);
+			        imagen2=(ImageView) findViewById(R.id.p3_img2);
+
+			        imagen1.setOnClickListener(new View.OnClickListener(){				
+						public void onClick(View v){
+							dialog.show();
+							i=1;
+						}
+					});		        
+			        imagen2.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();						
+							i=2;
+						}
+					});		        
+	    		break;
+	        	case R.id.plantilla4:
+	        		setContentView(R.layout.plantilla_4);
+			        imagen1=(ImageView) findViewById(R.id.p4_img1);
+			        imagen2=(ImageView) findViewById(R.id.p4_img2);
+			        imagen3=(ImageView) findViewById(R.id.p4_img3);
+			        imagen4=(ImageView) findViewById(R.id.p4_img4);
+			        
+			        imagen1.setOnClickListener(new View.OnClickListener(){				
+						public void onClick(View v){
+							dialog.show();
+							i=1;
+						}
+					});		        
+			        imagen2.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=2;
+						}
+					});
+			        imagen3.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=3;
+						}
+					});
+					imagen4.setOnClickListener(new View.OnClickListener(){							
+						public void onClick(View v) {
+							dialog.show();
+							i=4;
+						}
+					});
+	    		break;
+	        	case R.id.plantilla5:
+	        		setContentView(R.layout.plantilla_5);
+			        imagen1=(ImageView) findViewById(R.id.p5_img1);
+			        imagen2=(ImageView) findViewById(R.id.p5_img2);
+			        imagen3=(ImageView) findViewById(R.id.p5_img3);
+			        imagen4=(ImageView) findViewById(R.id.p5_img4);
+			        imagen5=(ImageView) findViewById(R.id.p5_img5);
+			        imagen6=(ImageView) findViewById(R.id.p5_img6);
+			        
+			        imagen1.setOnClickListener(new View.OnClickListener(){				
+						public void onClick(View v){
+							dialog.show();
+							i=1;
+						}
+					});		        
+			        imagen2.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=2;
+						}
+					});
+			        imagen3.setOnClickListener(new View.OnClickListener(){					
+						public void onClick(View v){
+							dialog.show();
+							i=3;
+						}
+					});
+					imagen4.setOnClickListener(new View.OnClickListener(){							
+						public void onClick(View v) {
+							dialog.show();
+							i=4;
+						}
+					});
+					imagen5.setOnClickListener(new View.OnClickListener(){						
+						public void onClick(View v){
+							dialog.show();
+							i=5;
+						}
+					});
+					imagen6.setOnClickListener(new View.OnClickListener(){						
+						public void onClick(View v){
+							dialog.show();
+							i=6;
+						}
+					});
+	    		break;	        
+	        }
+	        
+	        guardar = (Button) findViewById(R.id.guardar);
+			guardar.setOnClickListener(new OnClickListener(){
+				public void onClick(View arg0){
+	 				View ve = findViewById(R.id.comic);
+	 		        ve.setDrawingCacheEnabled(true);
+	 		        Bitmap b = ve.getDrawingCache();
+	 		        String extr = Environment.getExternalStorageDirectory().toString();
+	 		        File myPath = new File(extr);
+	 		        if (!myPath.exists()){
+	 		            boolean result = myPath.mkdir();
+	 		        } 		        
+	 		        myPath = new File(extr, getString(R.string.app_name) + ".jpg");
+	 		        FileOutputStream fos = null;
+	 		        try {
+	 		            fos = new FileOutputStream(myPath);
+	 		            b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+	 		            fos.flush();
+	 		            fos.close();
+	 		            MediaStore.Images.Media.insertImage(getContentResolver(), b,
+	 		                    "Screen", "screen");
+	 		        }
+	 		        catch (FileNotFoundException e) {
+	 		            Log.e("Error", e + "");
+	 		        }
+	 		        catch (Exception e) {
+	 		            Log.e("Error", e + "");
+	 		        } 		 		        
+	 		        crearDialogoAlerta("¡Enhorabuena!","¡Ya puedes mostrar tu comic!").show();
+				} 			
+			});
+			
+			compartir = (Button) findViewById(R.id.twitter);			
+			compartir.setOnClickListener(new OnClickListener(){
+				public void onClick(View arg){
+					intent = new Intent(P1_comic.this,AndroidTwitterActivity.class);		        
+					startActivity(intent);
+				} 			
+			});
+			
+			plus = (Button) findViewById(R.id.about);			
+			plus.setOnClickListener(new OnClickListener(){
+				public void onClick(View arg){
+					crearDialogoAlerta("Comic It!","Desarrollada por Ana Arias, Liliana Ramos y, Denny Schuldt.").show();
+				} 			
+			});
+		}
 		
+		/* Método: agregarDrag_and_Drop(ViewGroup vg, final ImageView img)
+		 * Uso: agregarDrag_and_Drop(vg,img);
+		 * ----------------------------------
+		 * Descripción: Permite agregar la funcionalidad de Dran and Drop tanto al
+		 * 				layout contenedor, como a la imagen dentro de el.
+		 */		
 		
 		
 		
