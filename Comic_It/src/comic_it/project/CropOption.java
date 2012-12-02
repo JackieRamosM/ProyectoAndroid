520e59c19b0e60f1610c601678f9a1d1e0ef8cd4
Ana Arias <acarias@espol.edu.ec>
2012-12-02 17:37:36 -0500

    Creando clase CropOption

---
+++ Comic_It/src/comic_it/project/CropOption.java
@@ -0,0 +1,10 @@
+package comic_it.project;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+public class CropOption {
+	public CharSequence title;
+	public Drawable icon;
+	public Intent appIntent;
+}
